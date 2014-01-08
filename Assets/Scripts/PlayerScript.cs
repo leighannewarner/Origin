d630778e4da97b48feca68f20ef9ffdc6d80a15f
leighannewarner <leighannewarner@gmail.com>
2014-01-07 23:05:54 -0800

    Falling off the edge of a platform
    Bug where the player can stick to the wall

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -22,23 +22,52 @@ public class PlayerScript : MonoBehaviour {
 	public int speedScaleFactor = 2;
 	public float sizeScaleFactor = 0.5f;
 
+	//Jumping
+	private bool jumping = false;
+	private bool falling = true;
+	private int jumpFrame = 0;
+	private int jumpCap = 15;
+
 	// Update is called once per frame
 	void Update () {
 		//Movement
 		float inputX = Input.GetAxis("Horizontal");
 		movement.x = speedVector.x * inputX;
 
-		//Growing and shrinking
+		/*if(jumping && jumpFrame < jumpCap) {
+			movement.y += speedVector.y * 0.05f;
+			jumpFrame++;
+
+			if(jumpFrame >= jumpCap) {
+				jumping = false;
+				movement.y = 0f;
+				falling = true;
+			}
+		}
+
+		if(falling && jumpFrame > 0) {
+			movement.y -= speedVector.y * 0.05f;
+			jumpFrame--;
+			
+			if(jumpFrame <= 0) {
+				movement.y = 0f;
+				falling = false;
+			}
+		}*/
+
+		if(falling) {
+			movement.y -= speedVector.y * 0.005f;
+		}
+
+		//User input
 		if(Input.GetMouseButtonDown(0) && size > 1) {
 			decreaseSize();
-			Debug.Log (speedVector);
 		} else if(Input.GetMouseButtonDown(1) && size < 3) {
 			increaseSize();
-			Debug.Log (speedVector);
 		}
 
 		if(Input.GetKeyUp("space")) {
-			Debug.Log ("Space key.");
+			jumping = true;
 		}
 	}
 
@@ -71,6 +100,23 @@ public class PlayerScript : MonoBehaviour {
 		} else {
 			speedVector.x = (50/speedScaleFactor);
 			speedVector.y = (50/speedScaleFactor);
+		}
+	}
+
+	void OnCollisionStay2D (Collision2D c) {
+		if(c.gameObject.name == "CollisionTile") {
+			falling = false;
+			//Debug.Log (c.gameObject.transform.position + " , " + this.gameObject.transform.position);
+			Debug.Log ("X: " + c.gameObject.transform.position.x + " , " + (this.gameObject.transform.position.x));
+			Debug.Log ("Y: " + c.gameObject.transform.position.y + " , " + (this.gameObject.transform.position.y));
+			Debug.Log ("Height: " + this.gameObject.transform.localScale);
+		}
+	}
+
+
+	void OnCollisionExit2D (Collision2D c) {
+		if(c.gameObject.name == "CollisionTile") {
+			falling = true;
 		}
 	}
 }
