c1cbbb3be9c5fd292d3478f4ecd919a4a0681f9c
leighannewarner <leighannewarner@gmail.com>
2014-04-08 15:04:53 -0700

    Began creating the Doge AI

---
+++ Assets/Scripts/DogeScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogeScript : MonoBehaviour {
+
+	void Awake()
+	{
+
+	}
+	
+	public void takeDamage(int d) {
+
+	}
+	
+	void OnCollisionEnter2D (Collision2D c) {
+
+	}
+	
+	void OnCollisionExit2D (Collision2D c) {
+
+	}
+}
